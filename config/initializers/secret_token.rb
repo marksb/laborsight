f0945ecc3f5c7edeb0cace07dd7c3850c34885bc
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-14 19:22:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DolChallenge::Application.config.secret_token = 'e651e9b23f883b0066ea5ce5512054eefab54e7f2b1847bf741507798ebe4a76fa43524428dd985e3f88071cf4ade036559750af4c59ebe2bd12a237dc5d179e'
