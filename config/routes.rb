d5616725a5c5148b98343d1dcb4ab2b3e15dca71
mbacksmeier <mbacksmeier@gmail.com>
2013-08-17 13:20:28 -0500

    Add Company routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 DolChallenge::Application.routes.draw do
   mount JasmineRails::Engine => "/specs" if defined?(JasmineRails)
+
+  get '/companies/data', to: 'companies#data', as: 'companies_data'
+  get '/companies/:id', to: 'companies#show', as: 'companies'
+
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -16,7 +16,7 @@ describe Company do
 
   mass_assignable = [:case_id, :trade_name, :legal_name, :flsa_cl_violtn_count, :flsa_cl_minor_count, :flsa_cl_cmp_assd_amt,
                      :flsa_cmp_assd_amt, :findings_start_date, :findings_end_date, :flsa_repeat_violator, :flsa_violation_count,
-                     :flsa_ee_atp_count, :bw_atp_amt, :flsa_mw_bw_atp_amt, :flsa_ot_bw_atp_amt, :flsa_15a3_bw_atp_amt]
+                     :flsa_ee_atp_count, :bw_atp_amt, :flsa_mw_bw_atp_amt, :flsa_ot_bw_atp_amt, :flsa_15a3_bw_atp_amt, :address_id, :industry_id]
 
   mass_assignable.each { | elem | it { should allow_mass_assignment_of(elem) } }
 
