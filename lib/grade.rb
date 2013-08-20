982e14d7d3ae16fa60c2c3d3781f15af761ba7df
keccers <kechampagne@gmail.com>
2013-08-20 14:58:48 -0500

    Refactor Grade module

--- lib/grade.rb
+++ lib/grade.rb
@@ -1,304 +1,157 @@
 module Grade
+  LOWER_LIMIT = 0
+
+  GRADING_SCALE = {
+    :flsa_cl_violtn_count => {
+      0             => 100,
+      1             => 60,
+      (2..100)      => 40
+    },
+    :flsa_cl_minor_count => {
+      0             => 100,
+      1             => 60
+    },
+    :flsa_cl_cmp_assd_amt => {
+      0             => 100,
+      (1..2000)     => 60,
+      (2001..5000)  => 40,
+      (5001..15000) => 20
+    },
+    :flsa_repeat_violator => {
+      "N/A"         => 100,
+      "R"           => 20,
+      "W"           => 20,
+      "RW"          => 10
+    },
+    :flsa_violation_count => {
+      0             => 100,
+      (1..50)       => 80,
+      (51..150)     => 60,
+      (151..300)    => 40
+    },
+    :flsa_ee_atp_count => {
+      0             => 100,
+      (1..50)       => 80,
+      (51..150)     => 60,
+      (151..300)    => 40
+    },
+    :bw_atp_amt => {
+      0             => 100,
+      (1..500)      => 80,
+      (501..5000)   => 60,
+      (5001..50000) => 40
+    },
+    :mspa_violtn_cnt => {
+      0              => 100,
+      (1..5)         => 60
+    },
+    :mspa_bw_atp_amt => {
+      0              => 100,
+      (1..2000)      => 80,
+      (2001..10000)  => 60,
+      (10001..25000) => 40
+    },
+    :mspa_ee_atp_cnt => {
+      0              => 100,
+      (1..20)        => 80,
+      (21..100)      => 60,
+      (101..200)     => 40
+    },
+    :mspa_cmp_assd_amt => {
+      0              => 100,
+      (1..500)       => 80,
+      (501..2000)    => 60,
+      (2001..7500)   => 40
+    },
+    :fmla_violtn_cnt => {
+      0              => 100,
+      (1..10)        => 80,
+      (11..100)      => 60,
+      (101..1000)    => 40
+    },
+    :fmla_bw_atp_amt => {
+      0              => 100,
+      (1..10)        => 80,
+      (11..100)      => 60,
+      (101..1000)    => 40
+    },
+    :fmla_ee_atp_cnt => {
+      0              => 100,
+      1              => 80,
+      (2..5)         => 60,
+      (6..25)        => 40
+    },
+    :fmla_cmp_assd_amt => {
+      0              => 100
+    },
+    :h1b_violtn_cnt  => {
+      0              => 100,
+      (1..10)        => 80,
+      (11.25)        => 60,
+      (26..100)      => 40
+    },
+    :h1b_bw_atp_amt  => {
+      0              => 100,
+      (1..10)        => 80,
+      (11..25)       => 60,
+      (26.100)       => 40
+    },
+    :h1b_ee_atp_cnt  => {
+      0              => 100,
+      (1..10000)     => 80,
+      (10001..50000) => 60,
+      (50001..15000) => 40
+    },
+    :h1b_cmp_assd_amt => {
+      0              => 100,
+      (1..1000)      => 80,
+      (1001..10000)  => 60,
+      (10001..50000) => 40
+    }
+  }
 
   def calculate_rating
-    child_labor_avg = (child_labor_count_rating + child_labor_minor_count_rating + child_labor_penalty_rating)/3
+    child_labor_avg = (flsa_cl_violtn_count_grade + flsa_cl_minor_count_grade + flsa_cl_cmp_assd_amt_grade)/3
 
-    mspa_avg = (mspa_violtn_cnt_rating + mspa_bw_atp_amt_rating + mspa_ee_atp_cnt_rating + mspa_cmp_assd_amt_rating)/4
+    mspa_avg = (mspa_violtn_cnt_grade + mspa_bw_atp_amt_grade + mspa_ee_atp_cnt_grade + mspa_cmp_assd_amt_grade)/4
 
-    fmla_avg = (fmla_violtn_cnt_rating + fmla_bw_atp_amt_rating + fmla_ee_atp_cnt_rating)/3
+    fmla_avg = (fmla_violtn_cnt_grade + fmla_bw_atp_amt_grade + fmla_ee_atp_cnt_grade)/3
 
-    h1b_avg = (h1b_violtn_cnt_rating + h1b_bw_atp_amt_rating + h1b_ee_atp_cnt_rating + h1b_cmp_assd_amt_rating)/4
+    h1b_avg = (h1b_violtn_cnt_grade + h1b_bw_atp_amt_grade + h1b_ee_atp_cnt_grade + h1b_cmp_assd_amt_grade)/4
 
-    total = child_labor_avg + (flsa_repeat_violator_rating * 2 )+ flsa_violation_count_rating + flsa_ee_atp_count_rating +
-            bw_atp_amt_rating + mspa_avg + fmla_avg + h1b_avg
+    total = child_labor_avg + (flsa_repeat_violator_grade * 2 )+ flsa_violation_count_grade + flsa_ee_atp_count_grade +
+            bw_atp_amt_grade + mspa_avg + fmla_avg + h1b_avg
     grade = total / 9
+  end
+
+  def method_missing(method_sym, *args, &block)
+    column = method_sym.to_s.sub("_grade","").to_sym
+
+    if method_sym.to_s =~ /_grade/ && GRADING_SCALE[column]
+      column_value = self.send(column)
+
+      GRADING_SCALE[column].each do |key, value|
+        return value if column_value == key || key.is_a?(Range) && key.include?(column_value)
+      end
+
+      return LOWER_LIMIT
+    end
+
+    super(method_sym, *args, &block)
   end
 
   def assign_letter_grade
     case calculate_rating
-      when 5
+      when (90..100)
         "A"
-      when 4
+      when (80..89)
         "B"
-      when 3
+      when (70..79)
         "C"
-      when 2
+      when (60..69)
         "D"
-      when 1
+      when (0..59)
         "F"
     end
   end
-
-private
-
-  def child_labor_count_rating
-    case self.flsa_cl_violtn_count
-      when 0
-        5
-      when 1
-        4
-      when (2..10)
-        3
-      when (11..50)
-        2
-      else
-        1
-    end
-  end
-
-  def child_labor_minor_count_rating
-    case self.flsa_cl_minor_count
-      when 0
-        5
-      when 1
-        3
-      else
-        1
-    end
-  end
-
-  def child_labor_penalty_rating
-    case self.flsa_cl_cmp_assd_amt
-      when 0
-        5
-      when (1..2000)
-        4
-      when (2001..5000)
-        3
-      when (5001..15000)
-        2
-      else
-        1
-    end
-  end
-
-  def flsa_repeat_violator_rating
-    case self.flsa_repeat_violator
-      when "N/A"
-        5
-      when "R"
-        2
-      when "RW"
-        1
-      else
-        5
-    end
-  end
-
-  def flsa_violation_count_rating
-    case self.flsa_violation_count
-      when 0
-        5
-      when (1..50)
-        4
-      when (51..150)
-        3
-      when (151..300)
-        2
-      else
-        1
-    end
-  end
-
-  def flsa_ee_atp_count_rating
-    case self.flsa_ee_atp_count
-      when 0
-        5
-      when (1..50)
-        4
-      when (51..150)
-        3
-      when (151..300)
-        2
-      else
-        1
-    end
-  end
-
-  def bw_atp_amt_rating
-    case self.bw_atp_amt
-      when 0
-        5
-      when (1..500)
-        4
-      when (501..5000)
-        3
-      when (5001..50000)
-        2
-      else
-        1
-    end
-  end
-
-  def mspa_violtn_cnt_rating
-    case self.mspa_violtn_cnt
-      when 0
-        5
-      when (1..5)
-        3
-      else
-        1
-    end
-  end
-
-  def mspa_bw_atp_amt_rating
-    case self.mspa_bw_atp_amt
-      when 0
-        5
-      when (1..2000)
-        4
-      when (2001..10000)
-        3
-      when (10001..25000)
-        2
-      else
-        1
-    end
-  end
-
-  def mspa_ee_atp_cnt_rating
-    case self.mspa_ee_atp_cnt
-      when 0
-        5
-      when (1..20)
-        4
-      when (21..100)
-        3
-      when (101..200)
-        2
-      else
-        1
-    end
-  end
-
-  def mspa_cmp_assd_amt_rating
-    case self.mspa_cmp_assd_amt
-      when 0
-        5
-      when (1..500)
-        4
-      when (501..2000)
-        3
-      when (2001..7500)
-        2
-      else
-        1
-    end
-  end
-
-  def fmla_violtn_cnt_rating
-    case self.fmla_violtn_cnt
-      when 0
-        5
-      when (1..10)
-        4
-      when (11..100)
-        3
-      when (101..1000)
-        2
-      else
-        1
-    end
-  end
-
-  def fmla_bw_atp_amt_rating
-    case self.fmla_bw_atp_amt
-      when 0
-        5
-      when (1..10)
-        4
-      when (11..100)
-        3
-      when (101..1000)
-        2
-      else
-        1
-    end
-  end
-
-  def fmla_ee_atp_cnt_rating
-    case self.fmla_bw_atp_amt
-      when 0
-        5
-      when (1)
-        4
-      when (2..5)
-        3
-      when (6..25)
-        2
-      else
-        1
-    end
-  end
-
-  #All entries are 0?
-  def fmla_cmp_assd_amt_rating
-    case fmla_cmp_assd_amt
-      when 0
-        5
-      else
-        1
-    end
-  end
-
-  def h1b_violtn_cnt_rating
-    case h1b_violtn_cnt
-      when 0
-        5
-      when 1..10
-        4
-      when 11.25
-        3
-      when 26..100
-        2
-      else
-        1
-    end
-  end
-
-  def h1b_bw_atp_amt_rating
-    case h1b_bw_atp_amt
-      when 0
-        5
-      when 1..10
-        4
-      when 11.25
-        3
-      when 26..100
-        2
-      else
-        1
-    end
-  end
-
-  def h1b_ee_atp_cnt_rating
-    case h1b_ee_atp_cnt
-      when 0
-        5
-      when 1..10000
-        4
-      when 10001..50000
-        3
-      when 50000..150000
-        2
-      else
-        1
-    end
-  end
-
-  def h1b_cmp_assd_amt_rating
-    case h1b_ee_atp_cnt
-      when 0
-        5
-      when 1..1000
-        4
-      when 1001..10000
-        3
-      when 10001..50000
-        2
-      else
-        1
-    end
-  end
-
 end
--- spec/models/grade_spec.rb
+++ spec/models/grade_spec.rb
@@ -4,7 +4,7 @@ describe Grade do
   company = FactoryGirl.create(:company)
 
   it "generates a number score for the company" do
-    expect(company.calculate_rating).to eq(5)
+    expect(company.calculate_rating).to eq(100)
   end
 
   it "generates a letter grade for the company" do
