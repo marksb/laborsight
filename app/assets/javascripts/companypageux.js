d994c184ddb36ec94e6771739de7acfabaa58e3c
priyamathew <pri1229@gmail.com>
2013-08-22 14:07:43 -0500

    Move company page js to new js file

---
+++ app/assets/javascripts/companypageux.js
@@ -0,0 +1,24 @@
+function getCompanyInfo(id){
+  $.get('/companies/' + id + '/chart', function(response){
+    createChart();
+  });
+}
+
+function myTabs() {
+  $( "#pure-stats" ).tabs({
+  });
+};
+
+function stateOnClick(){
+  $('#state').on('click', function(event){
+    console.log("whatup");
+    event.preventDefault();
+    $(this).addClass('selected');
+  });
+};
+
+$( document ).ready(function() {
+  MapView.init();
+  myAccordion();
+  stateOnClick();
+});
