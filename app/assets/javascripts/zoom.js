5c39e75d9de1877ad8c026a16eaf3bf3c14d3f55
keccers <kechampagne@gmail.com>
2013-09-26 18:17:59 -0400

    Style zoom controls and update search controls to match

--- app/assets/javascripts/zoom.js
+++ app/assets/javascripts/zoom.js
@@ -3,29 +3,49 @@ function ZoomControl(div, map) {
   // Get the control DIV. We'll attach our control UI to this DIV.
   var controlDiv = div;
 
-  // We set up a variable for the 'this' keyword since we're adding event
-  // listeners later and 'this' will be out of scope.
-  controlDiv.style.padding = '5px';
-  controlDiv.style.backgroundColor = "#FFFFFF"
+  controlDiv.style.margin = '28px 0px 0px 22px';
+  controlDiv.style.cursor = 'pointer';
+  controlDiv.style.border = "1px solid #9e9e9e"
+  controlDiv.style.opacity = "0.8";
+  controlDiv.style.backgroundColor = "#FFFFFF";
+  controlDiv.style.fontFamily = 'Open Sans';
+  controlDiv.style.borderRadius = '3px';
+  controlDiv.style.height = '36px';
+  controlDiv.style.width = '72px';
 
   // Set CSS for the control border.
   var zoomout = document.createElement('div');
   zoomout.title = 'Click to zoom out';
+  zoomout.style.display = "inline-block"
+  zoomout.style.borderRight = "1px solid #9e9e9e"
+  zoomout.style.width = '50%';
+  zoomout.style.height = '100%';
   controlDiv.appendChild(zoomout);
 
-  // Set CSS for the control interior.
+  // Set text for the zoom out button.
   var zoomoutText = document.createElement('div');
-  zoomoutText.innerHTML = '<strong>---</strong>';
+  zoomoutText.innerHTML = '<strong>-</strong>';
+  zoomoutText.style.fontSize = '40px';
+  zoomoutText.style.marginTop = '3px';
+  zoomoutText.style.textAlign = 'center';
+  zoomoutText.style.color = "#9e9e9e"
   zoomout.appendChild(zoomoutText);
 
   // Set CSS for the setHome control border.
   var zoomin = document.createElement('div');
   zoomin.title = 'Click to zoom in';
+  zoomin.style.display = "inline-block"
+  zoomin.style.width = '50%';
+  zoomin.style.height = '100%';
   controlDiv.appendChild(zoomin);
 
   // Set CSS for the control interior.
   var zoominText = document.createElement('div');
-  zoominText.innerHTML = '<strong>+++++</strong>';
+  zoominText.innerHTML = '<strong>+</strong>';
+  zoominText.style.fontSize = '30px';
+  // zoominText.style.marginTop = '2px';
+  zoominText.style.textAlign = 'center';
+  zoominText.style.color = "#9e9e9e"
   zoomin.appendChild(zoominText);
 
   // Setup the click event listener for Home:
