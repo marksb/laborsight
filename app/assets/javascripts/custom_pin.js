63287cabdc3349e389068950c90d884124012997
keccers <kechampagne@gmail.com>
2013-09-27 00:49:46 -0400

    Add logic to CustomPin controller to show pins based on NAICS code

--- app/assets/javascripts/custom_pin.js
+++ app/assets/javascripts/custom_pin.js
@@ -2,12 +2,22 @@ function CustomPin(naic) {
 
   this.image ='/assets/maps/markerRed.png';
   var naic = naic;
+  var restaurants = new RegExp(/72\d{4}/);
+  var construction = new RegExp(/23\d{4}/);
 
-  if (naic === 722211)
+
+  if (restaurants.test(naic))
     {
-    this.image = '/assets/maps/marker2.png';
-    };
-   
+    this.image = '/assets/maps/marker3.png';
+    }
+  else if (construction.test(naic))
+    {
+    this.image = '/assets/maps/fast-food-24.png';
+    }
+   else 
+    {
+    this.image ='/assets/maps/markerRed.png';
+    }  
 
   this.getImage = function(){
     return this.image
