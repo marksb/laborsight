4258cb0e714a2334d2875041d7f217d4ebbe5810
keccers <kechampagne@gmail.com>
2013-10-08 21:29:37 -0400

    Rolls back custom pin logic

--- app/assets/javascripts/custom_pin.js
+++ app/assets/javascripts/custom_pin.js
@@ -1,24 +1,6 @@
 function CustomPin(naic) {
 
-  this.image ='/assets/maps/markerRed.png';
-  var naic = naic;
-  var restaurants = new RegExp(/72\d{4}/);
-  var construction = new RegExp(/23\d{4}/);
-
-
-  if (restaurants.test(naic))
-    {
-    this.image = '/assets/maps/marker3.png';
-    }
-  else if (construction.test(naic))
-    {
-    this.image = '/assets/maps/fast-food-24.png';
-    }
-   else 
-    {
-    this.image ='/assets/maps/markerRed.png';
-    }  
-
+  this.image ='/assets/maps/markerRed.png'; 
   this.getImage = function(){
     return this.image
   }
